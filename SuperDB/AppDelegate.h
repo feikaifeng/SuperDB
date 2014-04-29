8435c5ab9be66601ce04f69dbe265ba903e6d38d
feikaifeng <feikaifeng@gmail.com>
2014-04-29 17:02:17 +0800

    Initial Commit

---
+++ SuperDB/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SuperDB
+//
+//  Created by feikaifeng on 14-4-29.
+//  Copyright (c) 2014年 symg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ SuperDB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SuperDB
+//
+//  Created by feikaifeng on 14-4-29.
+//  Copyright (c) 2014年 symg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
