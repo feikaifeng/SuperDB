38286cb63114c8b38394984009a858810c50c9eb
feikaifeng <feikaifeng@gmail.com>
2014-04-29 17:42:36 +0800

    初始化

---
+++ SuperDB/HeroListController.h
@@ -0,0 +1,13 @@
+//
+//  HeroListController.h
+//  SuperDB
+//
+//  Created by feikaifeng on 14-4-29.
+//  Copyright (c) 2014年 symg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeroListController : UIViewController<UITableViewDelegate,UITableViewDataSource>
+
+@end
